81bdc053a45fdc4692cf7adcf15ec9ae76eb3fb4
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-04-22 12:14:57 +0300

    1 Created class Person.
    class Person describe field firstName, lastName.

---
+++ CW01_CLApp_01/CLApp/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLApp.Models
+{
+    public class Person
+    {
+        public string firstName { get; set; }
+        public string secondName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            secondName = lastName;
+        }
+
+        public override string ToString() => $"{firstName} {secondName}";
+    }
+}
--- CW01_CLApp_01/CLApp/Program.cs
+++ CW01_CLApp_01/CLApp/Program.cs
@@ -1,11 +1,16 @@
-﻿namespace CLApp
+﻿using CLApp.Models;
+
+namespace CLApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Start project!");
 
+            Person kopo = new Person("Kostiantyn", "Polishko");
+
+            Console.WriteLine(kopo);
         }
     }
 }
